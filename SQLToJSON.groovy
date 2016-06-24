9501aa352dad514326b3ac1c18d0dd462fc149d7
Lea Marie Labagala <lea.labagala@gmail.com>
2016-06-24 17:48:04 +0800

    Added SQLToJSON but not functioning

---
+++ SQLToJSON.groovy
@@ -0,0 +1,30 @@
+@Grab('mysql:mysql-connector-java:5.1.25')
+@GrabConfig(systemClassLoader = true)
+
+import groovy.sql.Sql
+import java.sql.SQLRecoverableException
+
+Sql sql = null;
+try
+{
+   sql = Sql.newInstance('jdbc:mysql://localhost:3306/groovytests', 'root', 'apollo', 'com.mysql.jdbc.Driver') 
+}
+catch (SQLRecoverableException recoverableEx)
+{
+   println "SQLRecoverableException: ${recoverableEx}"
+   println "\tError Code: ${recoverableEx.getErrorCode()}"
+   println "\tSQL Status Code: ${recoverableEx.getSQLState()}"
+   System.exit(-1)
+}
+
+def xml = new groovy.json.JsonBuilder()
+xml.Monsters
+{
+   sql.eachRow("select * from monsters") { row ->
+      Characters
+      {
+         name(row.name)
+         age(row.age)
+      }
+   }
+}
